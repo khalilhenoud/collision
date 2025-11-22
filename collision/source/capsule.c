131ccc0d6168b25caa50e28b2ea092d574968ec2
Khalil Henoud <khalilhenoud@gmail.com>
2025-11-22 15:08:22 -0500

    updates

--- collision/source/capsule.c
+++ collision/source/capsule.c
@@ -1,12 +1,12 @@
 /**
  * @file capsule.c
  * @author khalilhenoud@gmail.com
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-06-18
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include <math.h>
 #include <assert.h>
@@ -17,25 +17,25 @@
 
 sphere_capsule_classification_t
 classify_sphere_capsule(
-  const sphere_t *source, 
+  const sphere_t *source,
   const capsule_t *capsule,
   vector3f* penetration)
 {
-  vector3f a_capsule, b_capsule, on_capsule; 
-  
+  vector3f a_capsule, b_capsule, on_capsule;
+
   assert(penetration != NULL);
 
   get_capsule_segment_loose(capsule, &a_capsule, &b_capsule);
   on_capsule = closest_point_on_segment_loose(
-    &source->center, 
-    &a_capsule, 
+    &source->center,
+    &a_capsule,
     &b_capsule);
 
   {
     sphere_t sphere_on_capsule = { on_capsule, capsule->radius };
     spheres_classification_t classification = classify_spheres(
       source,
-      &sphere_on_capsule, 
+      &sphere_on_capsule,
       penetration);
 
     switch (classification) {
@@ -55,30 +55,30 @@ classify_sphere_capsule(
       assert(0);
       break;
     }
-  } 
+  }
 
-  return SPHERES_DISTINCT;
+  return SPHERE_CAPSULE_DISTINCT;
 }
 
 int32_t
 classify_capsules_segments(
-  const capsule_t *source, 
-  const capsule_t *target, 
+  const capsule_t *source,
+  const capsule_t *target,
   segment_t *result)
 {
-  vector3f a_source, b_source, a_target, b_target; 
+  vector3f a_source, b_source, a_target, b_target;
 
   assert(result != NULL);
-  
+
   get_capsule_segment_loose(source, &a_source, &b_source);
   get_capsule_segment_loose(target, &a_target, &b_target);
-  
+
   {
     // find the closest points on the capsules.
     segment_t segments[3] = {{ a_source, b_source }, { a_target, b_target }};
     segments_classification_t classification = classify_segments(
-      segments + 0, 
-      segments + 1, 
+      segments + 0,
+      segments + 1,
       segments + 2);
     *result = segments[2];
     return classification;
@@ -87,15 +87,15 @@ classify_capsules_segments(
 
 capsules_classification_t
 classify_capsules(
-  const capsule_t *source, 
-  const capsule_t *target, 
+  const capsule_t *source,
+  const capsule_t *target,
   vector3f* penetration)
 {
   segment_t bridge;
-  segments_classification_t 
+  segments_classification_t
   segments_classification = classify_capsules_segments(source, target, &bridge);
 
-  // special cases: it is up to the user code to handle these cases. call 
+  // special cases: it is up to the user code to handle these cases. call
   // classify_capsules_segments to get the overlap.
   switch (segments_classification) {
   case SEGMENTS_IDENTICAL:
@@ -116,7 +116,7 @@ classify_capsules(
     sphere_t sphere_source = { bridge.points[0], source->radius };
     sphere_t sphere_target = { bridge.points[1], target->radius };
     spheres_classification_t spheres_classification = classify_spheres(
-      &sphere_source, 
+      &sphere_source,
       &sphere_target,
       penetration);
 
